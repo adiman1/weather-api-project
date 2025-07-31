9e177844e0b08bdd7dae40d33ad8dcc8a5f04051
aditya <110511924+adiman1@users.noreply.github.com>
2025-07-31 13:29:01 +0530

    Update append_data.py

--- src/append_data.py
+++ src/append_data.py
@@ -7,10 +7,11 @@ from .helpers import split_localtime
 from .helpers import transform_and_append
 
 # Filepaths to store Medallion Structure
-BASE_DIR = "D:/Test/data/"
-RAW_DIR = os.path.join(BASE_DIR, "raw_data")
-STAGED_DIR = os.path.join(BASE_DIR, "staged_data")
-FINAL_DIR = os.path.join(BASE_DIR, "final_data")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(BASE_DIR, "data")
+RAW_DIR = os.path.join(DATA_DIR, "raw_data")
+STAGED_DIR = os.path.join(DATA_DIR, "staged_data")
+FINAL_DIR = os.path.join(DATA_DIR, "final_data")
 
 # To Raw Data (Bronze)
 def append_to_csv(df, filename=os.path.join(RAW_DIR, "weather_data.csv")):
